af7c5885995d9b12a8f14f8cf4381e4aebfc21f1
SamuelPDev <Samuelperalta129@gmail.com>
2022-05-23 16:10:13 -0400

    fist commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,57 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
-  },
-  getters: {
+    characters: [],
+    charactersFilter: [],
   },
   mutations: {
+    setCharacters(state, payload) {
+      state.characters = payload;
+    },
+    setCharactersFilters(state, payload) {
+      state.charactersFilter = payload;
+    },
   },
   actions: {
+    async getCharacters({ commit }) {
+      try {
+        const response = await fetch(
+          "https://rickandmortyapi.com/api/character"
+        );
+        const data = await response.json();
+        commit("setCharacters", data.results);
+        commit("setCharactersFilters", data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    filterByStatus({ commit, state }, status) {
+      const filter = state.characters.filter((character) => {
+        return character.status.includes(status);
+      });
+      commit("setCharactersFilters", filter);
+    },
+    filterByName({ commit, state }, name) {
+      const formatName = name.toLowerCase();
+      const filter = state.characters.filter((character) => {
+        const characterName = character.name.toLowerCase();
+        if (characterName.includes(formatName)) {
+          return character;
+        }
+      });
+      commit("setCharactersFilters", filter);
+    },
+    filterByName({commit, state}, name) {
+      const formatName = name.toLowerCase()
+      const filter = state.characters.filter((character) => {
+        const characterName = character.name.toLowerCase()
+        if(characterName.includes(formatName)) {
+          return character
+        }
+      })
+      commit('setCharactersFilters', filter)
+    }
   },
-  modules: {
-  }
-})
+  modules: {},
+});
